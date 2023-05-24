669b9a04e0dd29a5a085601e7bb156c9a09c72d9
Dominic <dominiclaercio@outlook.com>
2023-05-24 01:26:17 -0300

    Criação da Classe D

---
+++ D.h
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class D{
+    private:
+        int D1;
+        int D2;
+
+    public:
+        D(){
+            D1 = 0;
+            D2 = 0.0;
+        }
+        void setD1(int entry){
+            D1 = entry;
+        }
+        void setD2(int entry){
+            D2 = entry;
+        }
+        int getD1(){
+            return D1;
+        }
+        int getA2(){
+            return D2;
+        }
+        void MD1(){
+            cout << "MD1" << endl;
+        }
+        void MD2(){
+            cout << "MD2" << endl;
+        }
+};
