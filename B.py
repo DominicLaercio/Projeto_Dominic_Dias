c306b70d97c672c1d937f0f5bcc7644d8ba1d42b
DominicLaercio <93347514+DominicLaercio@users.noreply.github.com>
2023-05-23 22:56:07 -0300

    Create B.py

---
+++ B.py
@@ -0,0 +1,16 @@
+class B:
+    B1 = 0
+    B2 = 0.0
+    
+    def getB1():
+        return B1
+    
+    def getB2():
+        return B2
+    
+    def MB1():
+        print("MB1")
+        
+    def MB2():
+        print("MB2")
+        
