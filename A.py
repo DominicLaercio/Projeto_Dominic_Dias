51db9c98fba12e8d54a4cc7b9e6ad4dacb7a747c
DominicLaercio <93347514+DominicLaercio@users.noreply.github.com>
2023-05-23 22:55:41 -0300

    Create A.py

---
+++ A.py
@@ -0,0 +1,16 @@
+class A:
+    A1 = 0
+    A2 = 0.0
+    
+    def getA1():
+        return A1
+    
+    def getA2():
+        return A2
+    
+    def MA1():
+        print("MA1")
+        
+    def MA2():
+        print("MA2")
+        
